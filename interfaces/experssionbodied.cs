2b31818915f6f97f77bfe91e872ec467c7b70408
kkar2 <kkar2@LUFFY>
2026-06-01 15:06:50 +0530

    made changes

--- interfaces/experssionbodied.cs
+++ interfaces/experssionbodied.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace interfaces
-{
-    class ExpressionBodiedMembers
-    {
-        public void PrintMessage() => Console.WriteLine("Hello, World!");
+//namespace interfaces
+//{
+//    class ExpressionBodiedMembers
+//    {
+//        public void PrintMessage() => Console.WriteLine("Hello, World!");
 
-        public int square(int x) => x * x;
+//        public int square(int x) => x * x;
 
-        public ExpressionBodiedMembers() => Console.WriteLine("Constructor called!");
-    }
-    internal class experssionbodied
-    {
-        static void Main(string[] args)
-        {
-            ExpressionBodiedMembers obj= new ExpressionBodiedMembers();
-            obj.PrintMessage();
-            Console.WriteLine(obj.square(5));
-        }
-    }
-}
+//        public ExpressionBodiedMembers() => Console.WriteLine("Constructor called!");
+//    }
+//    internal class experssionbodied
+//    {
+//        static void Main(string[] args)
+//        {
+//            ExpressionBodiedMembers obj= new ExpressionBodiedMembers();
+//            obj.PrintMessage();
+//            Console.WriteLine(obj.square(5));
+//        }
+//    }
+//}
