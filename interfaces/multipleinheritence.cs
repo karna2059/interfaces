76c0b54db41da2acf8599f3c16948f91e8a84e19
kkar2 <kkar2@LUFFY>
2026-06-01 15:49:17 +0530

    made changes

--- interfaces/multipleinheritence.cs
+++ interfaces/multipleinheritence.cs
@@ -1,98 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace interfaces
-{
-    interface iphonebasic
-    {
-        void calling();
-        void recieving();
+//namespace interfaces
+//{
+//    interface iphonebasic
+//    {
+//        void calling();
+//        void recieving();
 
-        void sendingmessage();
+//        void sendingmessage();
 
-        void endcall();
-    }
-    class iphonese : iphonebasic
-    {
-        public void calling()
-        {
-            Console.WriteLine("iphonese calling");
-        }
-        public void endcall()
-        {
-            Console.WriteLine("iphonese end call");
-        }
-        public void recieving()
-        {
-            Console.WriteLine("iphonese recieving");
-        }
-        public void sendingmessage()
-        {
-            Console.WriteLine("iphonese sending message");
-        }
-    }
+//        void endcall();
+//    }
+//    class iphonese : iphonebasic
+//    {
+//        public void calling()
+//        {
+//            Console.WriteLine("iphonese calling");
+//        }
+//        public void endcall()
+//        {
+//            Console.WriteLine("iphonese end call");
+//        }
+//        public void recieving()
+//        {
+//            Console.WriteLine("iphonese recieving");
+//        }
+//        public void sendingmessage()
+//        {
+//            Console.WriteLine("iphonese sending message");
+//        }
+//    }
 
-    interface iphoneadvance
-    {
-        void faceid();
-        void videocall();
+//    interface iphoneadvance
+//    {
+//        void faceid();
+//        void videocall();
 
-        void wirelesscharging();
-    }
+//        void wirelesscharging();
+//    }
 
-    class iphone11 : iphonebasic, iphoneadvance
-    {
-       public void calling()
-        {
-            Console.WriteLine("iphone11 calling");
-        }
-        public void endcall()
-        {
-            Console.WriteLine("iphone11 end call");
-        }
-        public void recieving()
-        {
-            Console.WriteLine("iphone11 recieving");
-        }
-        public void sendingmessage()
-        {
-            Console.WriteLine("iphone11 sending message");
-        }
-        public void faceid()
-        {
-            Console.WriteLine("iphone11 faceid");
-        }
-        public void videocall()
-        {
-            Console.WriteLine("iphone11 videocall");
-        }
-        public void wirelesscharging()
-        {
-            Console.WriteLine("iphone11 wirelesscharging");
-        }
-    }
+//    class iphone11 : iphonebasic, iphoneadvance
+//    {
+//       public void calling()
+//        {
+//            Console.WriteLine("iphone11 calling");
+//        }
+//        public void endcall()
+//        {
+//            Console.WriteLine("iphone11 end call");
+//        }
+//        public void recieving()
+//        {
+//            Console.WriteLine("iphone11 recieving");
+//        }
+//        public void sendingmessage()
+//        {
+//            Console.WriteLine("iphone11 sending message");
+//        }
+//        public void faceid()
+//        {
+//            Console.WriteLine("iphone11 faceid");
+//        }
+//        public void videocall()
+//        {
+//            Console.WriteLine("iphone11 videocall");
+//        }
+//        public void wirelesscharging()
+//        {
+//            Console.WriteLine("iphone11 wirelesscharging");
+//        }
+//    }
 
-    internal class multipleinheritence
-    {
-        static void Main(string[] args)
-        {
-            iphonebasic obj=new iphonese();
-            obj.calling();
-            obj.recieving();
-            obj.sendingmessage();
-            obj.endcall();
-            obj = new iphone11();
-            obj.calling();
-            obj.recieving();
-            obj.sendingmessage();
-            obj.endcall();
-            iphoneadvance obj1 = new iphone11();
-            obj1.faceid();
-            obj1.videocall();
-            obj1.wirelesscharging();
-        }
-    }
-}
+//    internal class multipleinheritence
+//    {
+//        static void Main(string[] args)
+//        {
+//            iphonebasic obj=new iphonese();
+//            obj.calling();
+//            obj.recieving();
+//            obj.sendingmessage();
+//            obj.endcall();
+//            obj = new iphone11();
+//            obj.calling();
+//            obj.recieving();
+//            obj.sendingmessage();
+//            obj.endcall();
+//            iphoneadvance obj1 = new iphone11();
+//            obj1.faceid();
+//            obj1.videocall();
+//            obj1.wirelesscharging();
+//        }
+//    }
+//}
---
+++ interfaces/paymentinterfaces.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaces
+{
+    using System;
+
+    namespace PaymentInterfaces
+    {
+       
+        interface IPaymentMethod
+        {
+            void Pay();
+            void CancelPayment();
+            void CheckBalance();
+        }
+
+       
+        class DebitCard : IPaymentMethod
+        {
+            public void Pay()
+            {
+                Console.WriteLine("Payment done using Debit Card");
+            }
+
+            public void CancelPayment()
+            {
+                Console.WriteLine("Debit Card payment cancelled");
+            }
+
+            public void CheckBalance()
+            {
+                Console.WriteLine("Checking Debit Card balance");
+            }
+        }
+
+      
+        interface IOnlinePayment
+        {
+            void PayPal();
+            void UPI();
+            void Wallet();
+        }
+
+     
+        class PayPalPayment : IPaymentMethod, IOnlinePayment
+        {
+            public void Pay()
+            {
+                Console.WriteLine("Online payment successful");
+            }
+
+            public void CancelPayment()
+            {
+                Console.WriteLine("Online payment cancelled");
+            }
+
+            public void CheckBalance()
+            {
+                Console.WriteLine("Checking online account balance");
+            }
+
+            public void PayPal()
+            {
+                Console.WriteLine("Payment through PayPal");
+            }
+
+            public void UPI()
+            {
+                Console.WriteLine("Payment through UPI");
+            }
+
+            public void Wallet()
+            {
+                Console.WriteLine("Payment through Wallet");
+            }
+        }
+
+      
+    internal class paymentinterfaces
+    {
+            static void Main(string[] args)
+            {
+                IPaymentMethod obj = new DebitCard();
+
+                obj.Pay();
+                obj.CheckBalance();
+                obj.CancelPayment();
+
+                Console.WriteLine();
+
+                obj = new PayPalPayment();
+
+                obj.Pay();
+                obj.CheckBalance();
+                obj.CancelPayment();
+
+                Console.WriteLine();
+
+                IOnlinePayment obj1 = new PayPalPayment();
+
+                obj1.PayPal();
+                obj1.UPI();
+                obj1.Wallet();
+            }
+        }
+    }
+}
